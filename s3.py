e9ac8465d5732da4dce413aa00406fd6d9473472
Savita Datta <savitadatta98@gmail.com>
2023-08-10 16:13:29 +1000

    add list of storage classes

--- s3.py
+++ s3.py
@@ -1,11 +1,12 @@
 import boto3
 
-# Let's use Amazon S3
+# check diff between client and resource
 s3 = boto3.resource('s3')
 
 for bucket in s3.buckets.all():
     print(bucket.name)
 
+# 'STANDARD'|'REDUCED_REDUNDANCY'|'STANDARD_IA'|'ONEZONE_IA'|'INTELLIGENT_TIERING'|'GLACIER'|'DEEP_ARCHIVE'|'OUTPOSTS'|'GLACIER_IR'|'SNOW'
 # s3.upload_file(PATH, BUCKET, KEY, ExtraArgs={'StorageClass': 'GLACIER_IR'})
 
 # data = open('test.jpg', 'rb')
